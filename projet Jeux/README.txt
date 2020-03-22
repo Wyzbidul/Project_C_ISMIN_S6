4de79aba5b3a8722107bd76fad5d0b860cf7a4c4
Bascraft <45175867+Bascraft@users.noreply.github.com>
2020-03-22 19:45:16 +0100

    Add files via upload

---
+++ projet Jeux/README.txt
@@ -0,0 +1,10 @@
+You must install these libs to make the programme work on your PC 
+
+sudo apt-get install libsdl1.2-dev
+sudo apt-get install libsdl-image1.2 libsdl-image1.2-dev
+
+
+DEV Comments:
+
+Put all your paths to files used in the main in c_cpp_propreties.json under "includePath".
+Or in you includes put the whole path in.
---
+++ projet Jeux/src/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"  
+ 
+void pause();
+ 
+int main()
+{
+    SDL_Init(SDL_INIT_VIDEO); // Initialisation de la SDL
+ 
+    SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
+    
+    pause(); // Mise en pause du programme
+ 
+    SDL_Quit(); // Arrêt de la SDL
+ 
+    return EXIT_SUCCESS; // Fermeture du programme
+}
+ 
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+ 
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
+}
